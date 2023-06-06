9aab85af96e6697ad26da28b7df643f2f79f8e27
DominickVN <116526606+DominickVN@users.noreply.github.com>
2023-06-06 00:38:15 -0700

    whisper
    added a whisperRoll command that sends the roll to your DMs. commands can continue to be called inside that DM.
    also made the default function of !roll be 1d20

--- diceCommands.py
+++ diceCommands.py
@@ -4,7 +4,7 @@ from bot import bot
 
 
 @bot.command()
-async def roll(ctx, dice_expression):
+async def roll(ctx, dice_expression='1d20'):
     try:
         parts = dice_expression.lower().split('+')  # Split the expression by '+'
         num_dice, num_sides = map(int, parts[0].split('d'))
@@ -19,5 +19,28 @@ async def roll(ctx, dice_expression):
             total += sum(modifiers)  # Add the modifiers to the total
 
         await ctx.send(f'```Rolling {dice_expression}: {", ".join(map(str, rolls))}\nTotal: {total}```')
+    except ValueError:
+        await ctx.send("Invalid dice expression. Please use the format XdY[+Z], where X is the number of dice, Y is the number of sides, and Z is an optional modifier.")
+
+@bot.command()
+async def whisperRoll(ctx, dice_expression='1d20'):
+    try:
+        parts = dice_expression.lower().split('+')  # Split the expression by '+'
+        num_dice, num_sides = map(int, parts[0].split('d'))
+        if num_dice <= 0 or num_sides <= 0:
+            raise ValueError
+        
+        rolls = [random.randint(1, num_sides) for _ in range(num_dice)]
+        total = sum(rolls)
+
+        if len(parts) > 1:
+            modifiers = list(map(int, parts[1:]))  # Extract the modifiers as a list of integers
+            total += sum(modifiers)  # Add the modifiers to the total
+
+        # Create a direct message channel with the sender
+        dm_channel = await ctx.author.create_dm()
+
+        # Send the roll result to the sender's DMs
+        await dm_channel.send(f'```Rolling {dice_expression}: {", ".join(map(str, rolls))}\nTotal: {total}```')
     except ValueError:
         await ctx.send("Invalid dice expression. Please use the format XdY[+Z], where X is the number of dice, Y is the number of sides, and Z is an optional modifier.")
