6864b07b0be8f9170d4a551e1a4bae9ded73d963
DominickVN <116526606+DominickVN@users.noreply.github.com>
2023-06-05 23:45:22 -0700

    first level spells
    finished the first level spells library, adjusted the spells command to:
    -include the first level spells library
    -capitalize all words if the spell has multiple

--- spellcommands.py
+++ spellcommands.py
@@ -1,5 +1,6 @@
 from bot import bot
 from cantrips_library import cantrips
+from first_level_library import first_level_spells
 
 
 
@@ -8,18 +9,27 @@ async def spell(ctx, *, spell_name):
     spell_name = spell_name.lower()  # Convert the spell name to lowercase for case-insensitive matching
     if spell_name in cantrips:
         spell_info = cantrips[spell_name]
-        response = f"# {spell_name.capitalize()}\n\n" \
-                f"```Level: {spell_info['level']}\n```" \
-                f"```* Casting Time: {spell_info['casting_time']}\n```" \
-                f"```* Range: {spell_info['range']}\n```" \
-                f"```* Components: {spell_info['components']}\n```" \
-                f"```* Material Components: {spell_info.get['material_components']}\n```" \
-                f"```* Duration: {spell_info['duration']}\n```" \
-                f"```* Concentration: {spell_info.get['concentration']}\n```" \
-                f"```* Ritual: {spell_info.get['ritual']}\n```" \
-                f"```* School: {spell_info['school']}\n```" \
-                f"```\n{spell_info['description']}```"
+    elif spell_name in first_level_spells:
+        spell_info = first_level_spells[spell_name]
     else:
         response = "Spell not found."
+        await ctx.send(response)
+        return
+
+    # Capitalize both words if the spell name contains a space
+    if ' ' in spell_name:
+        spell_name = spell_name.title()
+
+    response = f"# {spell_name}\n\n" \
+               f"```Level: {spell_info['level']}\n```" \
+               f"```* Casting Time: {spell_info['casting_time']}\n```" \
+               f"```* Range: {spell_info['range']}\n```" \
+               f"```* Components: {spell_info['components']}\n```" \
+               f"```* Material Components: {spell_info.get('material_components', '-')}\n```" \
+               f"```* Duration: {spell_info['duration']}\n```" \
+               f"```* Concentration: {spell_info.get('concentration', False)}\n```" \
+               f"```* Ritual: {spell_info.get('ritual', False)}\n```" \
+               f"```* School: {spell_info['school']}\n```" \
+               f"```\n{spell_info['description']}```"
 
     await ctx.send(response)
