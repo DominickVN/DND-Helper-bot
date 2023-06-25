6a640929695cc851a9f26592eaf726b812e0bcb1
DominickVN <116526606+DominickVN@users.noreply.github.com>
2023-06-25 13:36:38 -0700

    more npc names

--- npc_generator.py
+++ npc_generator.py
@@ -3,19 +3,29 @@ from bot import bot
 
 npc_races = {
     'human': {
-        'names': ['John', 'Mary', 'Robert', 'Emma', 'William', 'Olivia']
+        'male_names': ['Aseir', 'Bardeid', 'Haseid', 'Khemed', 'Mehmen', 'Sudeiman', 'Zasheir', 'Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd', 'Bor', 'Fodel', 'Glar', 'Grigor', 'Igan', 'Ivor', 'Kosef', 'Mival', 'Orel', 'Pavel', 'Sergor', 'Ander', 'Blath', 'Bran', 'Frath', 'Geth', 'Lander', 'Luth', 'Malcer', 'Stor', 'Taman', 'Urth'],
+        'female_names': ['Atala', 'Ceidil', 'Hama', 'Jasmal', 'Meilil', 'Seipora', 'Yasheira', 'Zasheida', 'Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele', 'Alethra', 'Kara', 'Katernin', 'Mara', 'Natali', 'Olma', 'Tana', 'Zora', 'Amafrey', 'Betha', 'Cefrey', 'Kethra', 'Mara', 'Olga', 'Silifrey', 'Westra'],
+        'surnames': ['Basha', 'Dumein', 'Jassan', 'Khalid', 'Mostana', 'Pashar', 'Rein', 'Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag', 'Bersk', 'Chernin', 'Dotsk', 'Kulenov', 'Marsk', 'Nemetsk', 'Shemov', 'Starag', 'Brightwood', 'Helder', 'Hornraven', 'Lackman', 'Stormwind', 'Windrivver']
     },
     'elf': {
-        'names': ['Lithian', 'Elysia', 'Thranduil', 'Elara', 'Galadriel', 'Aranion']
+        'male_names': ['Lithian', 'Thranduil', 'Aranion', 'Adran', 'Aelar', 'Aramil', 'Arannis', 'Aust', 'Beiro', 'Berrian', 'Carric', 'Enialis', 'Erdan', 'Erevan', 'Galinndan', 'Hadarai', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Laucian', 'Mindartis', 'Paelias', 'Peren', 'Quarion', 'Riardon', 'Rolen', 'Soveliss', 'Thamior', 'Tharivol', 'Theren', 'Varis'],
+        'female_names': ['Elysia', 'Elara', 'Galadriel', 'Adrie', 'Althaea', 'Anastrianna', 'Andraste', 'Antinua', 'Bethrynna', 'Birel', 'Caelynn', 'Drusilia', 'Enna', 'Felosial', 'Ielenia', 'Jelenneth', 'Keyleth', 'Leshanna', 'Lia', 'Meriele', 'Mialee', 'Naivara', 'Quelenna', 'Quillathe', 'Sariel', 'Shanairra', 'Shava', 'Silaqui', 'Theirastra', 'Thia', 'Vadania', 'Valanthe', 'Xanaphia'],
+        'surnames': ['Amakiir (Gemflower)', 'Amastacia (Starflower)', 'Galanodel (Moonwhisper)', 'Holimion (Diamonddew)', 'Ilphelkiir (Gemblossom)', 'Liadon (Silverfrond)', 'Meliamne (Oakenheel)', 'Naïlo (Nightbreeze)', 'Siannodel (Moonbrook)', 'Xiloscient (Goldpetal)']
     },
     'dwarf': {
-        'names': ['Thorin', 'Gimli', 'Faldor', 'Hilda', 'Durgan', 'Balin']
+        'male_names': ['Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Bruenor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Veit', 'Vondal'],
+        'female_names': ['Amber', 'Artin', 'Audhild', 'Bardryn', 'Dagnal', 'Diesa', 'Eldeth', 'Falkrunn', 'Finellen', 'Gunnloda', 'Gurdis', 'Helja', 'Hlin', 'Kathra', 'Kristryd', 'Ilde', 'Liftrasa', 'Mardred', 'Riswynn', 'Sannl', 'Torbera', 'Torgga', 'Vistra'],
+        'surnames': ['Balderk', 'Battlehammer', 'Brawnanvil', 'Dankil', 'Fireforge', 'Frostbeard', 'Gorunn', 'Holderhek', 'Ironfist', 'Loderr', 'Lutgehr', 'Rumnaheim', 'Strakeln', 'Torunn', 'Ungart']
     },
     'halfling': {
-        'names': ['Pippin', 'Merry', 'Samwise', 'Rosie', 'Frodo', 'Lily']
+        'male_names': ['Pippin', 'Merry', 'Samwise', 'Alton', 'Ander', 'Cade', 'Corrin', 'Eldon', 'Errich', 'Finnan', 'Garret', 'Lindal', 'Lyle', 'Merric', 'Milo', 'Osborn', 'Perrin', 'Reed', 'Roscoe', 'Wellby'],
+        'female_names': ['Rosie', 'Lily', 'Andry', 'Bree', 'Callie', 'Cora', 'Euphemia', 'Jillian', 'Kithri', 'Lavinia', 'Lidda', 'Merla', 'Nedda', 'Paela', 'Portia', 'Seraphina', 'Shaena', 'Trym', 'Vani', 'Verna'],
+        'surnames': ['Brushgather', 'Goodbarrel', 'Greenbottle', 'High-hill', 'Hilltopple', 'Leagallow', 'Tealeaf', 'Thorngage', 'Tosscobble', 'Underbough']
     },
     'orc': {
-        'names': ['Grommash', 'Durotan', 'Gul\'dan', 'Garrosh', 'Zugzug', 'Thrall']
+        'male_names': ['Grommash', 'Durotan', 'Gul\'dan', 'Dench', 'Feng', 'Gell', 'Henk', 'Holg', 'Imsh', 'Keth', 'Krusk', 'Mhurren', 'Ront', 'Shump', 'Thokk'],
+        'female_names': ['Baggi', 'Emen', 'Engong', 'Kansif', 'Myev', 'Neega', 'Ovak', 'Ownka', 'Shautha', 'Sutha', 'Vola', 'Volen', 'Yevelda'],
+        'surnames': []
     }
 }
 
@@ -25,37 +35,35 @@ async def npc(ctx, action=None, race=None, gender=None):
         if race and race.lower() in npc_races:
             race = race.lower()
         else:
-            race = 'human'
+            race = random.choice(list(npc_races.keys()))
 
-        if gender:
-            gender = gender.lower()
-            if gender not in ['male', 'female']:
-                await ctx.send('Invalid gender. Please specify "male" or "female" or leave it blank for a random gender.')
-                return
+        if gender and gender.lower() not in ['male', 'female']:
+            gender = None
 
         name = generate_name(race, gender)
+        surname = generate_surname(race)
         age = random.randint(18, 60)
         ability_scores = generate_ability_scores()
 
-        npc_info = f"Name: {name}\nAge: {age}\nRace: {race.title()}"
-        if gender:
-            npc_info += f"\nGender: {gender.capitalize()}"
-        npc_info += f"\nAbility Scores: {ability_scores}"
+        npc_info = f"Name: {name} {surname}\nAge: {age}\nGender: {gender.capitalize()}\nAbility Scores: {ability_scores}\nRace: {race.title()}"
 
         await ctx.send(f"Generated NPC:\n```{npc_info}```")
 
 def generate_name(race, gender):
-    if race in npc_races:
+    if race == 'human':
         if gender == 'male':
-            names = npc_races[race]['names'][:3]
+            names = npc_races[race]['male_names']
         elif gender == 'female':
-            names = npc_races[race]['names'][3:]
+            names = npc_races[race]['female_names']
         else:
-            names = npc_races[race]['names']
+            names = npc_races[race]['male_names'] + npc_races[race]['female_names']
     else:
-        names = npc_races['human']['names']
-
+        names = npc_races[race]['names']
     return random.choice(names)
+
+def generate_surname(race):
+    surnames = npc_races[race]['surnames']
+    return random.choice(surnames) if surnames else ''
 
 def generate_ability_scores():
     scores = [random.randint(8, 12) for _ in range(6)]
