a89aa893d7e571c08c1347b595a43c261e535b13
DominickVN <116526606+DominickVN@users.noreply.github.com>
2023-06-30 15:19:56 -0700

    prefix
    & is now the prefix

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", case_insensitive=False, intents = discord.Intents.all() )
+bot = commands.Bot(command_prefix="&", case_insensitive=False, intents = discord.Intents.all() )
