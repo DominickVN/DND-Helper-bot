7dfd11665f32cc68d08ddd6c7c524d45ca214cf5
DominickVN <116526606+DominickVN@users.noreply.github.com>
2023-07-02 19:39:31 -0700

    actions
    Added the Actions command

---
+++ actionsCommands.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from bot import bot
+from libraries.actions_library import actions
+
+@bot.command(aliases=['act'])
+async def action(ctx, action_name):
+    action_name = action_name.capitalize()
+    if action_name in actions:
+        action_info = actions[action_name]
+        description = action_info["Description"]
+        usage = action_info["Usage"]
+
+        embed = discord.Embed(title=f"{action_name} Action", color=discord.Color.blue())
+        embed.add_field(name="Description", value=description, inline=False)
+        embed.add_field(name="Usage", value=usage, inline=False)
+
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("That action is not found in the library.")
---
+++ libraries/actions_library.py
@@ -0,0 +1,54 @@
+actions = {
+    "Attack": {
+        "Description": "You can make a melee or ranged attack against a target creature within your reach or range. The attack roll and damage dealt depend on your weapon or spell.",
+        "Usage": "Specify the target creature and roll an attack roll. If the attack hits, roll damage and apply it to the target."
+    },
+    "Spell": {
+        "Description": "You can cast a spell using your spellcasting abilities. Spells have various effects and require specific components or actions to cast.",
+        "Usage": "Choose a spell from your available spells and follow the casting instructions provided in the spell description."
+    },
+    "Dash": {
+        "Description": "You focus your energy on moving swiftly. The Dash action allows you to move additional distance on your turn.",
+        "Usage": "Use the Dash action to double your movement speed for the turn."
+    },
+    "Disengage": {
+        "Description": "You can use the Disengage action to safely move away from an enemy without provoking opportunity attacks.",
+        "Usage": "Declare that you're using the Disengage action and move away from the enemy without triggering opportunity attacks."
+    },
+    "Dodge": {
+        "Description": "You can focus on avoiding attacks, granting you advantage on Dexterity saving throws and imposing disadvantage on attack rolls against you.",
+        "Usage": "Declare that you're using the Dodge action. Until the start of your next turn, attackers have disadvantage on attack rolls against you, and you have advantage on Dexterity saving throws."
+    },
+    "Grapple": {
+        "Description": "You can attempt to grapple a creature, restraining its movement and granting you control over it.",
+        "Usage": "Make a special melee attack roll contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check. If you succeed, the target is grappled."
+    },
+    "Help": {
+        "Description": "You can lend your aid to another creature in the completion of a task. You take a Help action on your turn to grant advantage to an ally on their next ability check or attack roll.",
+        "Usage": "Use the Help action and specify the target creature you want to assist."
+    },
+    "Hide": {
+        "Description": "You can attempt to hide yourself from others, making it harder for them to detect your presence.",
+        "Usage": "Declare that you're using the Hide action and make a Dexterity (Stealth) check to hide. The result determines whether you successfully hide from others."
+    },
+    "Improvise": {
+        "Description": "You can attempt to perform an action not covered by the standard D&D rules, using your creativity and the DM's discretion.",
+        "Usage": "Describe the action you want to attempt, and the DM will determine how to resolve it based on the situation."
+    },
+    "Ready": {
+        "Description": "You can choose to ready an action, allowing you to take it later in response to a specific trigger.",
+        "Usage": "Declare that you're using the Ready action and specify the trigger condition for your action. When the trigger occurs, you can take your readied action."
+    },
+    "Search": {
+        "Description": "You can use the Search action to thoroughly examine an area or object, looking for hidden items or information.",
+        "Usage": "Declare that you're using the Search action and describe the area or object you want to search. The DM will provide you with any relevant findings."
+    },
+    "Shove": {
+        "Description": "You can use the Shove action to push a creature, attempting to knock it prone or move it away from you.",
+        "Usage": "Make a special melee attack roll contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check. If you succeed, you can push the target away or knock it prone."
+    },
+    "Object": {
+        "Description": "You can use an object in your environment or from your inventory to perform a specific action or function.",
+        "Usage": "Specify the object you want to use and describe how you're using it. The DM will determine the outcome based on the situation and object's properties."
+    }
+}
